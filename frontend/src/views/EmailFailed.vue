42b5da110a3a5ac79c31fc5a92e4a37282b98025
jy_lee <swjddusdl921@gmail.com>
2025-05-26 00:08:50 +0900

    이메일 인증 끝 ㅠㅠ

--- frontend/src/views/EmailFailed.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container text-center mt-5">
-    <h2>❌ 인증에 실패했습니다.</h2>
-    <p class="mt-3">링크가 만료되었거나 잘못되었습니다.</p>
-    <router-link to="/" class="btn btn-outline-secondary mt-4">홈으로</router-link>
-  </div>
-</template>
